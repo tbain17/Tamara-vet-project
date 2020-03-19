508df0fa929bed93ca4ed7bf248841f8337cf395
Tamara Bain <tamara@tamaras-mbp.lan>
2020-03-19 14:57:31 +0000

    Added sql and seed data

---
+++ db/seeds.rb
@@ -0,0 +1,146 @@
+require_relative( "../models/owner" )
+require_relative( "../models/vet" )
+require_relative( "../models/pet" )
+require("pry-byebug")
+
+# Pet.delete_all()
+# Owner.delete_all()
+# Vet.delete_all()
+
+vet1 = Vet.new (
+  {
+    'name' => ''
+  }
+)
+vet2 = Vet.new (
+  {
+    'name' => ''
+  }
+)
+vet3 = Vet.new (
+  {
+    'name' => ''
+  }
+)
+
+vet1.save()
+vet2.save()
+vet3.save()
+
+owner1 = Owner.new(
+  {
+    'name' => ''
+  }
+)
+owner2 = Owner.new(
+  {
+    'name' => ''
+  }
+)
+owner3 = Owner.new(
+  {
+    'name' => ''
+  }
+)
+
+owner1.save()
+owner2.save()
+owner3.save()
+
+pet1 = Pet.new(
+  {
+    'name' => '',
+    'DoB' => '',
+    'vet_id' => vet1.id,
+    'owner_id' => owner2.id,
+    'type' => '',
+    'notes' => ''
+  }
+)
+
+pet2 = Pet.new(
+  {
+    'name' => '',
+    'DoB' => '',
+    'vet_id' => vet1.id,
+    'owner_id' => owner2.id,
+    'type' => '',
+    'notes' => ''
+  }
+)
+
+pet3 = Pet.new(
+  {
+    'name' => '',
+    'DoB' => '',
+    'vet_id' => vet1.id,
+    'owner_id' => owner2.id,
+    'type' => '',
+    'notes' => ''
+  }
+)
+
+pet4 = Pet.new(
+  {
+    'name' => '',
+    'DoB' => '',
+    'vet_id' => vet1.id,
+    'owner_id' => owner2.id,
+    'type' => '',
+    'notes' => ''
+  }
+)
+
+pet5 = Pet.new(
+  {
+    'name' => '',
+    'DoB' => '',
+    'vet_id' => vet1.id,
+    'owner_id' => owner2.id,
+    'type' => '',
+    'notes' => ''
+  }
+)
+
+pet6 = Pet.new(
+  {
+    'name' => '',
+    'DoB' => '',
+    'vet_id' => vet1.id,
+    'owner_id' => owner2.id,
+    'type' => '',
+    'notes' => ''
+  }
+)
+
+pet7 = Pet.new(
+  {
+    'name' => '',
+    'DoB' => '',
+    'vet_id' => vet1.id,
+    'owner_id' => owner2.id,
+    'type' => '',
+    'notes' => ''
+  }
+)
+
+pet8 = Pet.new(
+  {
+    'name' => '',
+    'DoB' => '',
+    'vet_id' => vet1.id,
+    'owner_id' => owner2.id,
+    'type' => '',
+    'notes' => ''
+  }
+)
+
+
+pet1.save()
+pet2.save()
+pet3.save()
+pet4.save()
+pet5.save()
+pet6.save()
+pet7.save()
+pet8.save()
---
+++ db/vet_practice.sql
@@ -0,0 +1,26 @@
+DROP TABLE pets;
+DROP TABLE owners;
+DROP TABLE vets;
+
+CREATE TABLE vets
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null
+);
+
+CREATE TABLE owners
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null
+);
+
+CREATE TABLE pets
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null,
+  DoB VARCHAR(255),
+  vet_id INT REFERENCES vets(id) ON DELETE CASCADE,
+  owner_id INT REFERENCES owners(id) ON DELETE CASCADE,
+  type VARCHAR(255),
+  notes VARCHAR(255)
+);
