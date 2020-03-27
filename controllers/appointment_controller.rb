f3b8a28aaa9f79003637614e960458f881d183e1
Tamara Bain <tamara@tamaras-mbp.lan>
2020-03-27 09:29:36 +0000

    tidied up controller files

--- controllers/appointment_controller.rb
+++ controllers/appointment_controller.rb
@@ -1,8 +1,6 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
-require_relative('../models/owner')
-require_relative('../models/vet')
 require_relative('../models/pet')
 require_relative('../models/room')
 require_relative('../models/appointment')
--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -2,11 +2,6 @@ require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('../models/owner')
-require_relative('../models/vet')
-require_relative('../models/pet')
-require_relative('../models/room')
-require_relative('../models/appointment')
-require_relative('../models/treatment')
 also_reload('../models/*')
 
 get '/owners' do
